267765c22ea79da6a5c53a04d196e1b2025c2e7a
Shiplayer <lllytep199515@yandex.ru>
2015-02-26 00:14:05 +0300

    Poker init

--- Poker/Player.cpp
+++
@@ -1,7 +0,0 @@
-#include "Player.h"
-
-int Player::count = 1;
-
-Player::Player() : MIN_CARDS(2), MAX_CARDS(5) {
-	name = "Player" + count;
-}
--- Poker/g++.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-
-g++ -Wall -std=c++11 *.cpp -o run
